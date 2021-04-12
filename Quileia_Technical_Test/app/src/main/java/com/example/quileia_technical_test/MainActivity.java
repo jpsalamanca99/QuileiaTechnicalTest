0ed1c9ae6fcd63803f9adf1ecf63a90b690079af
Chikerita <48142553+Chikerita@users.noreply.github.com>
2021-04-12 13:05:28 -0500

    Creacion de los layouts
    Se crearon las primeras versiones de los layouts de las listas de medicos y pacientes

--- Quileia_Technical_Test/app/src/main/java/com/example/quileia_technical_test/MainActivity.java
+++ Quileia_Technical_Test/app/src/main/java/com/example/quileia_technical_test/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.quileia_technical_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button patients;
+    private Button medics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        patients = findViewById(R.id.buttonPatients);
+        medics = findViewById(R.id.buttonMedics);
+
+        patients.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Patients.class);
+                startActivity(intent);
+            }
+        });
+        medics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Medics.class);
+                startActivity(intent);
+            }
+        });
     }
 }
