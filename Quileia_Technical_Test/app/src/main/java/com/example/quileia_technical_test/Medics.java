e82f6c888701339d155111f51333fa1ce32aa8ee
Chikerita <48142553+Chikerita@users.noreply.github.com>
2021-04-12 12:39:32 -0500

    Creacion de activitys
    Se crearon las activitys que mostraran las listas de pacientes y de medicos

---
+++ Quileia_Technical_Test/app/src/main/java/com/example/quileia_technical_test/Medics.java
@@ -0,0 +1,14 @@
+package com.example.quileia_technical_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Medics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medics);
+    }
+}
